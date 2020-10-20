3280d105091016f52085226ba1d8d04ed5071d8c
conacc <65893507+conacc@users.noreply.github.com>
2020-10-20 09:08:27 +0100

    Add files via upload

---
+++ 2. Turtle Graphics/turtleIntro.py
@@ -0,0 +1,11 @@
+# turtleIntro.py
+# @coneill 15/09/2020 
+# This program introduces Turtle Graphics
+
+from turtle import *
+
+shape('turtle') # This makes the shape of a turtle
+color('blue')   # 
+forward(50)
+right(90)
+forward(50)
