77ce19793aeae3c3a40e6b81cad3c8718850236b
conacc <65893507+conacc@users.noreply.github.com>
2020-10-21 10:19:11 +0100

    Add files via upload

---
+++ 2. Turtle Graphics/turtleTriangle.py
@@ -0,0 +1,11 @@
+# turtleTriangle.py
+# @coneill 22/09/2020
+# Draws a triangle in Turtle Graphics
+from turtle import *
+shape('turtle')
+color('red')
+forward(100)
+left(120)
+forward(100)
+left(120)
+forward(100)
