eaa0ff9c15e2aff2ec649d1f0a7e8aad73b6f259
conacc <65893507+conacc@users.noreply.github.com>
2021-02-10 11:58:00 +0000

    Add files via upload

---
+++ 7. Lists/replaceZerosActivity.py
@@ -0,0 +1,12 @@
+# replaceZerosActivity.py
+# @coneill 10/02/2021
+# Replace zeros in a list
+
+# Create a list with ten numbers, at least three zeros
+
+# Print the list to the screen
+
+# Use a for loop to go through the list and replace any
+# zeros with the number 100
+
+# Print the list to the screen again to show it works
