1efea7846974559fd1d82fde121957934649d7cc
conacc <65893507+conacc@users.noreply.github.com>
2021-03-25 12:25:33 +0000

    Update pandasColumnSlice.py

--- 8. Analytics/pandasColumnSlice.py
+++ 8. Analytics/pandasColumnSlice.py
@@ -1,3 +1,8 @@
 # pandasColumnSlice.py
 # @coneill 18/03/2021
+import pandas
 # Slice columns out of a dataframe
+fifa_df = pandas.read_csv('FIFA21-player-list.csv')
+# Select a number of columns - all rows
+fifa_df1 = fifa_df[['short_name', 'age', 'value_eur']]
+print(fifa_df1)
