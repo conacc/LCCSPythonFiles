15b893369ea14db99212e17ae094569c0c88d36b
conacc <65893507+conacc@users.noreply.github.com>
2021-05-18 08:25:02 +0100

    Add files via upload

---
+++ Practical Exams/Practice Practical Exam 1/Question 2.py
@@ -0,0 +1,4 @@
+# Question 2
+# Name:
+
+squared_numbers=[]
---
+++ Practical Exams/Practice Practical Exam 1/Question1.py
@@ -0,0 +1,12 @@
+# Question 1(a)
+# Name:
+
+num1 = 9
+num2 = 5
+
+choice = str(input('Do you want to (a)dd or (s)ubtract?: '))
+
+if choice == 'a':
+    print(num1+num2)
+elif choice == 's':
+    print(num1-num2)
