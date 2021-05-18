4f9d43025ced0f48762e434b4153c0a5f0717169
conacc <65893507+conacc@users.noreply.github.com>
2021-05-18 20:07:22 +0100

    Add files via upload

---
+++ Practical Exams/Practical Exam Summer 2021/Question12.py
@@ -0,0 +1,9 @@
+# Question 12
+# Student name:
+
+student_name = input('Please enter the students name: ')
+student_score = int(input('Please enter the students mark: '))
+score_as_a_percentage=(student_score/150)*100
+
+
+print(student_name,'scored',score_as_a_percentage,'%')
