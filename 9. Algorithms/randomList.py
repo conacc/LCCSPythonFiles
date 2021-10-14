1f18be69aec1e19de4e5419bff62a7e411b9ac21
conacc <65893507+conacc@users.noreply.github.com>
2021-10-14 10:06:33 +0100

    Add files via upload

---
+++ 9. Algorithms/randomList.py
@@ -0,0 +1,11 @@
+# randomList.py
+# @coneill 12/10/2021
+# Create a random list
+import random
+randomList = []
+for i in range (100):
+    r = random.randint(1,100)
+    randomList.append(r)
+print(randomList)
+    
+    
