227f5ce06486f2c8aeca8d758abf0ed96bcb897e
conacc <65893507+conacc@users.noreply.github.com>
2021-10-19 14:50:37 +0100

    Update timingSort.py

--- 9. Algorithms/timingSort.py
+++ 9. Algorithms/timingSort.py
@@ -8,21 +8,18 @@ randomList = []
 for i in range (100): 
     r = random.randint(1,100) 
     randomList.append(r) 
-print(randomList)
+print('Random List: ',randomList)
 
 # Timing of Selection Sort
 import time
 start = time.time()
 myList = randomList
 for index in range(len(myList)-1):
-    print(myList)
     nextMinValue = min(myList[index+1:])
     if nextMinValue < myList[index]:
         nextMinIndex = myList.index(nextMinValue)
         myList[nextMinIndex] = myList[index]
         myList[index] = nextMinValue
-print(myList)
-
 end = time.time()
 selectionTime = end-start
 print('Selection sort time: ',selectionTime,'seconds')
@@ -31,14 +28,12 @@ print('Selection sort time: ',selectionTime,'seconds')
 start = time.time()
 myList = randomList
 for index in range(1, len(myList)):
-    print(myList)
     itemInsert = myList[index]
     position = index
     while position > 0 and myList[position-1] > itemInsert:
         myList[position] = myList[position-1]
         position -=1
     myList[position] = itemInsert
-print(myList)
 end = time.time()
 insertionTime = end-start
 print('Insertion sort time: ',insertionTime,'seconds')
@@ -47,13 +42,11 @@ print('Insertion sort time: ',insertionTime,'seconds')
 start = time.time()
 myList = randomList
 for outerIndex in range(len(myList) - 1):
-    print(myList)
     for index in range(len(myList) - 1):
         if myList[index] > myList[index+1]:
             tempValue = myList[index]
             myList[index] = myList[index+1]
             myList[index+1] = tempValue
-print(myList)
 end = time.time()
 bubbleTime = end-start
 print('Bubble sort time: ',bubbleTime,'seconds')
@@ -76,7 +69,6 @@ def quickSort(listIn):
         return quickSort(belowPiv) + [pivot] + quickSort(abovePiv)
     else:
         return listIn
-print(quickSort(randomList))
 end = time.time()
 quickTime = end-start
 print('Quicksort time: ',quickTime,'seconds')
