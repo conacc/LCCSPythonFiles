fd7173ae6f0415bdf0f268704992479886dc74e8
conacc <65893507+conacc@users.noreply.github.com>
2021-11-10 15:49:19 +0000

    Add files via upload

---
+++ 2. Turtle Graphics/turtleFlag.py
@@ -0,0 +1,16 @@
+# turtleFlag.py
+from turtle import *
+shape('turtle')
+width(50)
+penup()
+goto(-250,250)
+flagList = ['red','orange','yellow','green','blue','indigo']
+for item in flagList:
+    color(item)
+    pendown()
+    forward(500)
+    penup()
+    right(90)
+    forward(50)
+    left(90)
+    backward(500)
