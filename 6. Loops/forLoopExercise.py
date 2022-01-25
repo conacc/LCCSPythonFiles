dfc44479919299c7cbf8c27da348887fe180148f
conacc <65893507+conacc@users.noreply.github.com>
2022-01-25 10:47:10 +0000

    Add files via upload

---
+++ 6. Loops/forLoopExercise.py
@@ -0,0 +1,18 @@
+# forLoopExercise.py
+# @coneill 25/01/2022
+# Predict, run, then complete comments explaining each line
+
+# Set the count to start at zero
+count = 0
+# 
+sentence = "Hello World, how are you?"
+# 
+for letter in sentence
+    if letter == " ":
+        # 
+        count+=1
+    elif letter == "?"
+        # 
+        count-=1
+#         
+print(count)
