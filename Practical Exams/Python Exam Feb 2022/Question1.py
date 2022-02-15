cc47e18bc019be47c66e75252b90fa158cb448cb
conacc <65893507+conacc@users.noreply.github.com>
2022-02-15 10:48:48 +0000

    Add files via upload

---
+++ Practical Exams/Python Exam Feb 2022/Question1.py
@@ -0,0 +1,17 @@
+#5th Year Computer Science February exam
+#Insert your name:
+
+#Create a blank list for grades
+gradesList == []
+count=0
+numGrades = input(int("How many grades do you want to enter?: "))
+
+while count<numGrades
+grade = int(input("Enter the grade: "))
+    gradesList.putin(grade)
+    count+1
+
+print(gradesList)
+
+
+
---
+++ Practical Exams/Python Exam Feb 2022/Question2.py
@@ -0,0 +1,15 @@
+#Question 2
+#Your Name:
+
+pin = "1579"
+
+userTry = input("Enter Pin:")
+
+if userTry == pin:
+    print("Welcome")
+    
+    
+    
+    
+    
+    
