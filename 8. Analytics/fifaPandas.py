6b076b3b40167baf0012d2c9d56eb96db1b953cc
conacc <65893507+conacc@users.noreply.github.com>
2022-04-07 13:24:56 +0100

    Update fifaPandas.py

--- 8. Analytics/fifaPandas.py
+++ 8. Analytics/fifaPandas.py
@@ -16,17 +16,24 @@ print('Shape(rows,cols):', fifaDF.shape)
 nameAgeValueDF=fifaDF[['short_name','age','value_eur']]
 print(nameAgeValueDF)
 
+# Create new DF with one type of player
 irishPlayersDF=fifaDF[fifaDF['nationality']=='Republic of Ireland']
 print(irishPlayersDF)
-
 under30DF=fifaDF[fifaDF['age']<30]
 print(under30DF)
 
+# Basic Statistics
+print('Youngest player age:',fifaDF['age'].min())
+print('Oldest player age:',fifaDF['age'].max())
+print('Mean player age:',fifaDF['age'].mean())
+print('Median player age:',fifaDF['age'].median())
 
-
-
-
-
-
-
-
+# Count players by age
+ageCount = fifaDF['age'].value_counts()
+print(ageCount)
+# Count players by Country Represented
+countryCount = fifaDF['nationality'].value_counts()
+print(countryCount)
+# Count players by Club Represented
+clubCount = fifaDF['club_name'].value_counts()
+print(clubCount)
