6c224724b1fe00ca8a74df9f038531ba638143d1
conacc <65893507+conacc@users.noreply.github.com>
2022-05-24 10:53:01 +0100

    Add files via upload

---
+++ Practical Exams/pythonPractice.py
@@ -0,0 +1,7 @@
+# practicalExamPractice.py
+name = input("Enter your name:")
+print("Hello",name,"how are you?")
+# Amend code so it asks for a Maths result
+# Print out the result and a grade
+# 70-100% Excellent, 40-70% Good
+# 0-40% Poor
