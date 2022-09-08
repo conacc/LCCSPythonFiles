cca22aa7a09cfe79e38bd2b1752b7524fee1cb88
conacc <65893507+conacc@users.noreply.github.com>
2022-09-08 17:18:33 +0100

    Add files via upload

---
+++ greenEggsAndHam.py
@@ -0,0 +1,21 @@
+# greenEggsAndHam.py
+# @coneill 08/09/2022
+
+print("I do not like green eggs and ham.")
+print("I do not like them Sam-I-am.")
+print()
+print("I do not like them here or there.")
+print("I do not like them anywhere.")
+print("I do not like them in a house")
+print("I do not like them with  a mouse")
+print()
+print("I do not like green eggs and ham.")
+print("I do not like them Sam-I-am.")
+print()
+print("I do not like them in a box")
+print("I do not like them with a fox")
+print("I will not eat them in the rain.")
+print("I will not eat them on a train")
+print()
+print("I do not like green eggs and ham.")
+print("I do not like them Sam-I-am.")
