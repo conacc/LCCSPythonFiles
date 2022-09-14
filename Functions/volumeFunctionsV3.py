90a3541f131962ca6fd3548d8a4d9493ebfebc26
conacc <65893507+conacc@users.noreply.github.com>
2022-09-14 14:19:30 +0100

    Add files via upload

---
+++ Functions/volumeFunctionsV3.py
@@ -0,0 +1,39 @@
+# volumeFunctions.py
+# @coneill 14/09/2022
+
+#Define function
+def volCube(sideIn):
+    volume = sideIn*sideIn*sideIn
+    return volume
+
+def volSphere(radiusIn):
+    volume = (4/3)*(3.14)*(radiusIn**3)
+    return volume
+
+def volCylinder(radiusIn,heightIn):
+    volume = (3.14)*(radiusIn**2)*(heightIn)
+    return volume
+# Call functions
+print('Welcome to the Volumator')
+choice = input('Enter 1 for Cube, 2 for Sphere, or 3 for Cylinder: ')
+choice = int(choice)
+if choice == 1:
+    print("Volume of Cube")
+    num = input('Enter side length of cube: ')
+    num = int(num) # convert input to an integer
+    output = volCube(num)
+    print(output)
+elif choice == 2:
+    print("Volume of Sphere")
+    num = input('Enter radius of sphere: ')
+    num = int(num) 
+    output = volSphere(num)
+    print(output)
+elif choice == 3:
+    print("Volume of Cylinder")
+    num1 = input('Enter radius of cylinder: ')
+    num1 = int(num1)
+    num2 = input('Enter height of cylinder: ')
+    num2 = int(num2) 
+    output = volCylinder(num1,num2)
+    print(output)
