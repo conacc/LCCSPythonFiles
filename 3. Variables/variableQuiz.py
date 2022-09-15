5653cd1976a5da0630a7dd7affd353f945acf361
conacc <65893507+conacc@users.noreply.github.com>
2022-09-15 08:29:15 +0100

    Update variableQuiz.py

--- 3. Variables/variableQuiz.py
+++ 3. Variables/variableQuiz.py
@@ -1,6 +1,6 @@
 # variableQuiz.py
 # @coneill 28/09/2020
-# Guess the varible type, then check your answer
+# Guess the varible type, fill in the blanks, then check your answers
 
 x = '6'  # x is  
 y = 6    # y is 
@@ -8,3 +8,6 @@ z = False # z is
 p = 2/3 # p is
 q = 7.0 # q is
 r = 'True' # r is
+
+# Print the type of each variable below
+print(type(x))
