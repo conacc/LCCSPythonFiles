e6e40f84408f7b915ba821c26666bad0550f3eb1
conacc <65893507+conacc@users.noreply.github.com>
2022-09-15 08:28:24 +0100

    Add files via upload

---
+++ 3. Variables/mathsOperations.py
@@ -0,0 +1,13 @@
+# mathsOperations.py
+# @coneill 14/09/2022
+
+a = 5
+b = 10
+c = 51
+d = 104
+
+print(a+b) # = 15 (addition)
+print(a-b) # = -5 (subtraction)
+print(b/a) # = 2.0 (division)
+print(d*b) # = 1040 (multiplication)
+print('&'*10)
---
+++ 3. Variables/variableTypes.py
@@ -0,0 +1,32 @@
+# variableTypes.py
+# @coneill 14/09/2022
+
+# Multiline Comment
+''' 
+num = 4 # create variable, assign value of 4
+print('Value of num:',num) # retreive value of num and print
+
+num = 7 # change value of num
+print('Value of num:',num)
+
+otherNum = 11
+print('Value of otherNum:',otherNum)
+
+num = otherNum
+print('Value of num:',num)
+'''
+
+a = 4
+b = 6.5
+c = -14
+
+print(type(a)) # Type: int
+print(type(b)) # Type: float
+print(type(c)) # Type: int
+
+d = 'John'
+print(type(d)) # Type: string
+
+e = False
+print(type(e))
+
