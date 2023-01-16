f6c6b016f2dace9ebb12db1a71039eea8931f921
conacc <65893507+conacc@users.noreply.github.com>
2023-01-16 14:52:24 +0000

    Create stringNameExercise.py

---
+++ 4. Strings/stringNameExercise.py
@@ -0,0 +1,7 @@
+# stringNameExercise.py
+# @coneill 16/01/2023
+# A program that asks user to enter their
+# name, then tells them how long it is,
+# what it starts and ends with, and
+# prints it out in all lower and all 
+# upper case
