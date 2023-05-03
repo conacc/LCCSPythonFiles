97b7b9b27f19b5a234eb1bb551eebb0f5044b194
conacc <65893507+conacc@users.noreply.github.com>
2023-05-03 11:50:03 +0100

    Add files via upload

---
+++ Functions/calcFunction.py
@@ -0,0 +1,18 @@
+# calcFunction.py
+# @coneill 03/05/2023
+
+# Define Functions
+def menu():
+    print("Welcome to the calculator.")
+    print("1. Square a number")
+    print("2. Add two numbers")
+    print("3. Add two numbers")
+    print("4. Odd or Even")
+    choice = input("Choose an Option: ")
+    return choice
+
+# Call Functions
+option = menu()
+
+if option == 1:
+    pass
