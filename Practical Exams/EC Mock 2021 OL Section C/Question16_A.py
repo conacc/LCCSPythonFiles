7e3cf4001c67676707e271368e4de9ed62fe731b
conacc <65893507+conacc@users.noreply.github.com>
2023-05-04 15:16:43 +0100

    Add files via upload

---
+++ Practical Exams/EC Mock 2021 OL Section C/Question16_A.py
@@ -0,0 +1,9 @@
+# Question16_A.py
+# Exam number:
+
+print("Welcome to the new online ordering system.\n")
+total_cost = 0
+item_count = 1
+price_of_item = float(input("Enter the price of item {}".format(item_count)+" : € "))
+total_cost+=price_of_item
+print("You entered",item_count,"items and the total is €",total_cost)
---
+++ Practical Exams/EC Mock 2021 OL Section C/Question16_B.py
@@ -0,0 +1,4 @@
+# Question16_B.py
+# Exam number:
+
+Standard_Postal_List = []
