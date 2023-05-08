5d12e920fa93064efcb5a8fc2ea120e5a3e41914
conacc <65893507+conacc@users.noreply.github.com>
2023-05-08 14:30:32 +0100

    Add files via upload

---
+++ Functions/calcFunctionV2.py
@@ -0,0 +1,66 @@
+# calcFunctionV2.py
+# @coneill 03/05/2023
+
+# Define Functions
+def menu():
+    print("Welcome to the calculator.")
+    print("1. Square a number")
+    print("2. Add two numbers")
+    print("3. Multiply two numbers")
+    print("4. Odd or Even")
+    choice = input("Choose an Option: ")
+    choice = int(choice) # cast String to int
+    return choice
+
+def squarer(num):
+    answer = num**2 # two stars for 'to the power of'
+    print("The square of your number is: ",answer)
+    
+def adder(num1, num2):
+    answer = num1+num2 # two stars for 'to the power of'
+    print("The sum of your numbers is: ",answer)
+
+def multiplier(num1, num2):
+    answer = num1*num2 # two stars for 'to the power of'
+    print("The product of your numbers is: ",answer)
+    
+def oddOrEven(num):
+    if num % 2 == 0:
+        ans = "Even"
+    elif num % 2 != 0:
+        ans = "Odd"
+    else:
+        ans = "Invalid Entry"
+    return ans
+
+# Call Functions
+option = menu()
+
+if option == 1:
+    userNum = input("Enter a number to square: ")
+    userNum = int(userNum) # cast String to int
+    squarer(userNum) # call function with argument userNum
+elif option == 2:
+    print("Enter two numbers to add: ")
+    userNum1 = input("First number: ")
+    userNum1 = int(userNum1)
+    userNum2 = input("Second number: ")
+    userNum2 = int(userNum2)
+    adder(userNum1, userNum2)
+elif option == 3:
+    print("Enter two numbers to multiply: ")
+    userNum1 = input("First number: ")
+    userNum1 = int(userNum1)
+    userNum2 = input("Second number: ")
+    userNum2 = int(userNum2)
+    multiplier(userNum1, userNum2)
+elif option == 4:
+    userNum = input("Enter a number: ")
+    userNum = int(userNum)
+    response = oddOrEven(userNum)
+    print(response)
+    
+    
+    
+    
+    
