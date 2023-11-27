b0573db8db6db16e95d885cfacbc753b65e3e4bd
conacc <65893507+conacc@users.noreply.github.com>
2023-11-27 12:24:49 +0000

    Update EC2022OL_Question_16A.py

--- Practical Exams/EC Mock 2022 OL Section C/EC2022OL_Question_16A.py
+++ Practical Exams/EC Mock 2022 OL Section C/EC2022OL_Question_16A.py
@@ -1,9 +1,13 @@
 # Question 16(a)
 # Student name:
+
 length = 6
 width = 4
+
+
 choice = input(“Do you want to fi nd the (p)erimeter or (a)rea? “)
-if choice == “p”:
+
+if choice == "p":
     print(length + length + width + width)
-elif choice == “a”:
+elif choice == "a":
     print(length*width)
