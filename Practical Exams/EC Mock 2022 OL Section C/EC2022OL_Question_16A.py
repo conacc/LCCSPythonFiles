13bd39b1f066edaae590d8ee4393ae34101920b1
conacc <65893507+conacc@users.noreply.github.com>
2023-11-27 12:22:42 +0000

    Add files via upload

---
+++ Practical Exams/EC Mock 2022 OL Section C/EC2022OL_Question_16A.py
@@ -0,0 +1,9 @@
+# Question 16(a)
+# Student name:
+length = 6
+width = 4
+choice = input(“Do you want to fi nd the (p)erimeter or (a)rea? “)
+if choice == “p”:
+    print(length + length + width + width)
+elif choice == “a”:
+    print(length*width)
---
+++ Practical Exams/EC Mock 2022 OL Section C/EC2022OL_Question_16B.py
@@ -0,0 +1,9 @@
+# Question 16(b)
+# Student name:
+
+import random
+
+computer_options = ["rock", "paper", "scissors"]
+
+
+computer_choice = computer_options (random.randint(0,2)]
