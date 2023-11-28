e7071af828a4e0b5628761bccdc04a7ca7cbdd38
conacc <65893507+conacc@users.noreply.github.com>
2023-11-28 10:21:57 +0000

    Add files via upload

---
+++ Practical Exams/EC Mock 2022 HL Section C/EC2022HL_Question_16A.py
@@ -0,0 +1,7 @@
+# Question 16(a)
+# Student name:
+import random
+your_name = input("Please enter your name: ")
+lucky_number = 5
+computer_die_roll = random.randint(1,6)
+print(“The computer rolled: “, computer_die_roll)
---
+++ Practical Exams/EC Mock 2022 HL Section C/EC2022HL_Question_16B.py
@@ -0,0 +1,23 @@
+# Question 16 (b)
+# Student name:
+import random
+ticket = []
+
+user_number = int(input("Please pick a number between 1 and 10: "))
+ticket.append(user_number)
+user_number = int(input("Please pick a number between 1 and 10: "))
+ticket.append(user_number)
+user_number = int(input("Please pick a number between 1 and 10: "))
+ticket.append(user_number)
+
+print("Your ticket is: ", ticket)
+print("The draw will start now, good luck!")
+drum = [1,2,3,4,5,6,7,8,9,10]
+result = []
+def lotto(ticket):
+    for times in range (3):
+        draw = drum[random.randint(0,len(drum))-1]
+        result.append(draw)
+        print("The draw was: ", result)
+ 
+lotto(ticket)
