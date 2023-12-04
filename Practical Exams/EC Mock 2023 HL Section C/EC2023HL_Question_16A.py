ac78a977aee70b477422679614ce652209d88006
conacc <65893507+conacc@users.noreply.github.com>
2023-12-04 10:12:17 +0000

    Add files via upload

---
+++ Practical Exams/EC Mock 2023 HL Section C/EC2023HL_Question_16A.py
@@ -0,0 +1,13 @@
+# Question 16(a)
+# Student Name:
+
+wages = int(input("Please enter your annual wages: ")) 
+
+cutoff = 36800  
+
+def income_tax(wages):
+    print("Welcome to my income tax calculator")
+
+
+
+income_tax(wages)
---
+++ Practical Exams/EC Mock 2023 HL Section C/EC2023HL_Question_16B.py
@@ -0,0 +1,15 @@
+# Question 16(b)
+# Student Name:
+
+
+import random
+
+faces = ["1","2","3","4","5","6","7","8","9","10","11","12","13"]
+
+suits = ["C","D","H","S"]
+
+player_face = faces[random.randint(0,12)]
+player_suit = suits[random.randint(0,3)]
+
+computer_face = faces[random.randint(0,12)]
+computer_suit = suits[random.randint(0,3)]
