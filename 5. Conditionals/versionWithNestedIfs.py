fa89b85f296209973642b032d3439913084062c6
conacc <65893507+conacc@users.noreply.github.com>
2023-12-18 14:02:19 +0000

    Update versionWithNestedIfs.py

--- 5. Conditionals/versionWithNestedIfs.py
+++ 5. Conditionals/versionWithNestedIfs.py
@@ -9,11 +9,19 @@ if choice == "N":
     num2 = input("Enter second number: ")
     num2 = int(num2)
     print("Numbers added", num1+num2)
+    if (num1+num2) % 2 == 0:
+        print("This number is even")
+    else:
+        print("This number is odd")
     
 elif choice == "W":
     word1 = input("Enter first word: ")
     word2 = input("Enter second word: ")
     print("Words added", word1+word2)
+    if (word1+word2).count("e") > 0 or (word1+word2).count("E") > 0:
+        print("This word contains an e")
+    else:
+        print("This word does not contain an e")
 
 else:
     print("That is not a valid entry")
