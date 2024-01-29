b9f8dbfaaf629f0955d9aa4cea420d712adc6061
conacc <65893507+conacc@users.noreply.github.com>
2024-01-29 12:53:58 +0000

    Add files via upload

---
+++ Practical Exams/EC Mock 2023 OL Section C/Question 16_A.py
@@ -0,0 +1,12 @@
+#Question 16(a)
+#Write your name here:
+option = input("Would you like to (a)dd or(s)ubtract? ")
+num1 = int(input("Please enter your first number: "))
+num2 = int(input("Please enter your second number: "))
+if option == "a":
+    print(num1 + num2)
+if option == "s":
+    print(num1 - num2)
+
+
+
