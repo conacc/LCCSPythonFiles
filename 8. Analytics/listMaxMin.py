5d944066b26c8fb1ba0d340f9f8cdc42ee47e1e4
conacc <65893507+conacc@users.noreply.github.com>
2024-04-18 09:02:56 +0100

    Add files via upload

---
+++ 8. Analytics/listMaxMin.py
@@ -0,0 +1,16 @@
+# listMaxMin.py
+
+myList = [4, 3, 6, 2, 10]
+
+# Sort list in ascending order
+myList.sort()
+
+# First index is minimum
+myMin = myList[0]
+
+# Final index is maximum
+myMax = myList[-1]
+
+# Print results
+print("Min:",myMin)
+print("Max:",myMax)
