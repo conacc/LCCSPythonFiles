d6de156011f654aa890cfbb8dd3f2b799db5316c
conacc <65893507+conacc@users.noreply.github.com>
2024-04-18 08:54:47 +0100

    Add files via upload

---
+++ 8. Analytics/statsModuleAverages.py
@@ -0,0 +1,10 @@
+# statsModuleAverages.py
+
+# Module is pre-installed in Thonny
+import statistics
+
+myList1 = [2, 4, 6, 8, 10]
+myMean = statistics.mean(myList1)
+print("Mean:",myMean)
+# The mode and median can be found
+# using the same module
