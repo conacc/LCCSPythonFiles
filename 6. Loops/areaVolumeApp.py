4835bfbc2111566effaddf4f1a5f1691375cf72b
conacc <65893507+conacc@users.noreply.github.com>
2025-01-21 09:35:32 +0000

    Create areaVolumeApp.py

---
+++ 6. Loops/areaVolumeApp.py
@@ -0,0 +1,27 @@
+# areaVolumeApp.py
+# @coneill 20/01/2025
+
+# Display menu
+print("*"*20)
+print("* Area/Volume App  *")
+print("*"*20)
+print("1. Area of circle")
+print("2. Vol of sphere")
+print("3. Vol of cube")
+print("4. Exit")
+print("*"*20)
+# User inputs choice
+choice = input("Enter option (1-4): ")
+choice = int(choice) # cast to integer
+# While loop
+while choice != 4: # != not equal to
+    if choice == 1:
+        pass
+    elif choice == 2:
+        pass
+    elif choice == 3:
+        pass
+    else:
+        pass
+    
+print("Goodbye")
