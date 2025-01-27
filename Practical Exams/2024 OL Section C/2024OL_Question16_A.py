711f7b7fe9ad90379ea8407349e195b99f034219
conacc <65893507+conacc@users.noreply.github.com>
2025-01-27 13:29:43 +0000

    Add files via upload

---
+++ Practical Exams/2024 OL Section C/2024OL_Question16_A.py
@@ -0,0 +1,9 @@
+# Question 16(a) 
+# Examination Number: 
+
+print("The program")
+
+age = int(input("What age are you? "))
+
+if (age >= 17):
+    print("You are entitled to apply for a driving licence.")
