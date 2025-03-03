86333a7f6a76c92c56f902e1e18dce422c9fae22
conacc <65893507+conacc@users.noreply.github.com>
2025-03-03 09:13:44 +0000

    Add files via upload

---
+++ HTML CSS JS/FileDownloadExample/script.js
@@ -0,0 +1,25 @@
+function myFunction() {
+  // Get the checkbox
+  var checkBox = document.getElementById("myCheck");
+  // Get the output text
+  var text = document.getElementById("text");
+
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+function download_txt() {
+  var textToSave = document.getElementById('text').innerHTML;
+  var hiddenElement = document.createElement('a');
+
+  hiddenElement.href = 'data:attachment/text,' + encodeURI(textToSave);
+  hiddenElement.target = '_blank';
+  hiddenElement.download = 'myFile.txt';
+  hiddenElement.click();
+}
+
+document.getElementById('test').addEventListener('click', download_txt);
