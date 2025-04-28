9561e5b6c2d47b4f039208ec9f483bcaa03c5a04
conacc <65893507+conacc@users.noreply.github.com>
2025-04-28 08:34:31 +0100

    Add files via upload

---
+++ Practical Exams/EC Mock 2025 OL Section C/Question16A_OL.py
@@ -0,0 +1,7 @@
+#Question_16_A_OL
+#Enter your name here:
+
+value_small_containers = 0.15
+
+number_small_containers = int(input("Please enter how many small containers you are returning: "))
+
---
+++ Practical Exams/EC Mock 2025 OL Section C/Question16B_OL.py
@@ -0,0 +1,10 @@
+#Question_16_B_OL
+#Enter your name here:
+
+import random
+print("Your options are as follows: d4, d6,d12 and d20")
+
+
+n = int(input("Which type of dice do you want? 4,6,12 or 20: "))
+
+
