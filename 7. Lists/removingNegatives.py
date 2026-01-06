a6b4e04fa8bb2afb5477d6cde0c4ddbb1db87424
conacc <65893507+conacc@users.noreply.github.com>
2026-01-06 13:43:07 +0000

    Create removingNegatives.py

---
+++ 7. Lists/removingNegatives.py
@@ -0,0 +1,12 @@
+# removingNegatives.py
+# @coneill 06/01/2026
+# Create a list of 10 integers, +ve and -ve
+myList = [-5,-4,-3,-2,-1,1,2,3,4,5]
+# Print the length, min and max of list
+print("Length:",len(myList))
+print("Max:",max(myList))
+print("Min:",min(myList))
+# Add two more numbers and print new list
+myList.append(-100)
+myList.append(100)
+print(myList)
