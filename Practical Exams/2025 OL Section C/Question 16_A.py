96a99973b46596e150c4b707f4ef22f82f0045c0
conacc <65893507+conacc@users.noreply.github.com>
2026-01-14 13:34:46 +0000

    Add files via upload

---
+++ Practical Exams/2025 OL Section C/Question 16_A.py
@@ -0,0 +1,11 @@
+# Question 16(a)
+# Examination Number:
+
+print("The program")
+
+length = float(input("What length is the room?: "))
+width = 20
+
+area = length * width
+
+print("The area of the floor is:", area)
